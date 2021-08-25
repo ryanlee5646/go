79b71daec1d3ece04536a7652e6f76348fd06863
Gyujin Lee <ryanlee5646@gmail.com>
2021-08-25 12:55:45 +0900

    20210825 TIL_go

---
+++ test3.go
@@ -0,0 +1,32 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	sum := 0
+// 	for i:=1 ; i <= 100; i++{
+// 		sum += i
+// 	}
+// 	fmt.Println(sum)
+// }
+
+// 무한루프
+// func main() {
+// 	for {
+// 		println("Hello")
+// 	}
+	
+// }
+
+// 배열선언
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+
+	for name := range nums{
+		println(name)
+	}
+	
+}
