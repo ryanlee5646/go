52cb999684d418449745e628f6fb6ed41fd307ac
Gyujin Lee <ryanlee5646@gmail.com>
2022-03-18 14:18:34 +0900

    20220318 golan TIL

--- main.go
+++ main.go
@@ -1,63 +1,7 @@
+// package 이름을 main으로 짓는 이유는 컴파일을 위함
+// 소스를 공개하고자 할때는 main.go를 만들지 않는다
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-
-// 함수 리턴값 지정
-func lenAndUpper (name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	fmt.Println("1")
-	return 
-}
-
-// 함수 다중 인자값
-func repeatMe(words ...string)([]string){
-	return words
-}
-
-// for문
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
-	}
-
-	return total
-}
-
-//if 문
-
-func canIDrink(age int) bool {
-	if koreanAge := age + 2 ; koreanAge < 18 {
-		return false
-	} 
-	return true
-	
-}
-
-//Swich 문
-
-func canIDrink2(age int) bool {
-	switch age {
-	case 10 :
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
 func main() {
-	result := superAdd(1,2,3,4,5,6)
-	fmt.Println(result)
-	fmt.Println(canIDrink(16))
-	fmt.Println(canIDrink2(18))
-	// a,b := lenAndUpper("hello world")
-	// fmt.Println(a,b)
-	// array := repeatMe("nico","lynn", "dal", "marl")
+	fmt.Println("hihi")
 }
